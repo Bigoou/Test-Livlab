0a7ba5e9e5b7fbb945920a85011ef497e1adcb06
Bigou <antoine.droyer@gmail.com>
2022-05-15 21:42:17 +0200

    List displaying + search input + filtering + validation button

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,132 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="search">
+      <input v-model="search" placeholder="Rechercher..."/>
+      <button v-on:click="filtering()">Valider</button>
+    </div>
+      <ul>
+        <li v-show="!isFiltered" class="card" v-for="i in list" :key="i">
+  <p>{{i}}</p>
+  </li>
+  <li v-show="isFiltered" class="card" v-for="i in filteredList" :key="i">
+  <p>{{i}}</p>
+  </li>
+      </ul>
+      
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      search : '',
+      list : ['Décathlon (H)', '110m haies (F)', '4x400m (F)', 'Poids (H)', 'France - Slovénie (Basket H)', 'Boxe - moins de 60 kg (F)', 'Canoë-kayak - K1 200m (H)', 'Omnium (H)', 'Keirin (F) Demi-finales', 'Australie - États-Unis (Football F)'],
+      isFiltered : false
+    }
+  },
+  computed: {
+    filteredList() {
+      console.log('trigger')
+          return this.list.filter(item => item.toLowerCase().includes(this.search.toLowerCase()))
+    }
+  },
+  methods: {
+    filtering() {
+      console.log(this.filteredList);
+      return this.isFiltered = true;
+    }
   }
 }
 </script>
 
 <style>
+
+* {
+  box-sizing: border-box;
+  margin : 0;
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #546bab;
+  height:100vh;
+  display:flex;
+  flex-direction:column;
+  justify-content: center;
+
 }
+
+.search {
+  display : inline;
+}
+
+
+
+button {
+margin-left : 10px;
+background-color: #131862;
+border: none;
+padding : 10px;
+color: white;
+border-radius : 4px;
+transition : all 0.1s ease-in-out;
+}
+
+button:hover {
+background-color: #1e258e;
+cursor : pointer;
+}
+
+ul {
+  list-style-type: none;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+}
+
+.card {
+  padding : 20px;
+  margin: 10px;
+  width: 200px;
+  height: 200px;
+  background-color: #131862;
+  border-radius: 4px;
+  box-shadow: 0 2px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(19, 24, 98, 0.24);
+  transition: all 0.2s ease-in-out;
+  color:white;
+}
+
+.card:hover{
+  transform : scale(1.03, 1.03)
+}
+
+input {
+  width: 30%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 2px solid #2c3e50;
+  background-color: #546bab;
+}
+
+input::placeholder {
+  color:#2c3e50;
+}
+
+input:focus {
+  outline:none;
+}
+
 </style>
